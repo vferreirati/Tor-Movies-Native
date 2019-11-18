3e6003c869749f2f000fab86c0b2820b277b9530
Victor <vferreirati@gmail.com>
2019-11-18 20:24:35 -0300

    navGraph initial setup

--- app/src/main/java/com/vferreirati/tormovies/ui/MainActivity.kt
+++ app/src/main/java/com/vferreirati/tormovies/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vferreirati.tormovies
+package com.vferreirati.tormovies.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vferreirati.tormovies.R
 
 class MainActivity : AppCompatActivity() {
 
