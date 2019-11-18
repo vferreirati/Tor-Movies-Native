28afdcb1293d526b4e7c81a9109df539bf35ef69
Victor <vferreirati@gmail.com>
2019-11-18 12:29:02 -0300

    add dagger extension methods

--- app/src/main/java/com/vferreirati/tormovies/MainActivity.kt
+++ app/src/main/java/com/vferreirati/tormovies/MainActivity.kt
@@ -1,42 +1,12 @@
 package com.vferreirati.tormovies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.vferreirati.tormovies.network.services.MoviesService
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
-            .build()
-        val service = Retrofit.Builder()
-            .baseUrl("https://tv-v2.api-fetch.website/")
-            .client(client)
-            .addConverterFactory(MoshiConverterFactory.create())
-            .build()
-            .create(MoviesService::class.java)
-
-        GlobalScope.launch {
-            try {
-                val response = service.queryMovies()
-                for(entry in response) {
-                    Log.d("TOR", entry.title)
-                }
-
-            } catch (e: Throwable) {
-                Log.e("TOR", "Error: ", e)
-            }
-        }
     }
 }
---
+++ app/src/main/java/com/vferreirati/tormovies/extensions/Activity.kt
@@ -0,0 +1,39 @@
+package com.vferreirati.tormovies.extensions
+
+import android.app.Activity
+import androidx.activity.viewModels
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.vferreirati.tormovies.TorApplication
+
+/**
+ * Método de extensão para obter o componente da aplicação
+ * */
+val Activity.injector get() = (application as TorApplication).applicationComponent
+
+/**
+ * Método de entexão para obter o [ViewModel] com escopo de [Activity] através de DI
+ * */
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : ViewModel> FragmentActivity.viewModel(
+    crossinline provider: () -> T
+) = viewModels<T> {
+    object: ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T = provider() as T
+    }
+}
+
+/**
+ * Método de entexão para obter determinado [ViewModel] com escopo de [Fragment] através de DI
+ * */
+@Suppress("UNCHECKED_CAST")
+inline fun <reified T : ViewModel> Fragment.viewmodel(
+    crossinline provider: () -> T
+) = viewModels<T> {
+    object: ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T = provider() as T
+    }
+}
