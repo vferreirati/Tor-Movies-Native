6c9097d661aa45e0266cc3d9673cf511edd3026a
Victor <vferreirati@gmail.com>
2019-12-12 19:54:44 -0300

    disable fullscreen

--- app/src/main/java/com/vferreirati/tormovies/ui/MainActivity.kt
+++ app/src/main/java/com/vferreirati/tormovies/ui/MainActivity.kt
@@ -10,7 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
