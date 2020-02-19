c1a02aee6f9f8199b129d3651af165ee397b364f
Victor Ferreira <vferreirati@gmail.com>
2020-02-19 11:27:51 -0300

    fix imagewrapper null and add hasAd property to torrent

--- app/src/main/java/com/vferreirati/tormovies/ui/adapter/TorrentAdapter.kt
+++ app/src/main/java/com/vferreirati/tormovies/ui/adapter/TorrentAdapter.kt
@@ -23,12 +23,15 @@ class TorrentAdapter(
 
     override fun onBindViewHolder(holder: TorrentHolder, position: Int) {
         val torrent = torrents[position]
+        val context = holder.itemView.context
+        val hasAdResId = if (torrent.hasAd) R.string.yes else R.string.no
 
         holder.txtFileSize.text = torrent.fileSize
         holder.txtTorrentQuality.text = torrent.quality
         holder.txtTorrentSource.text = torrent.source
         holder.txtPeerCount.text = torrent.peerCount.toString()
         holder.txtSeedCount.text = torrent.seedCount.toString()
+        holder.txtAdToDownload.text = context.getString(hasAdResId)
 
         holder.itemView.setOnClickListener { onClick(torrent.magneticUrl) }
     }
@@ -39,5 +42,6 @@ class TorrentAdapter(
         val txtPeerCount: AppCompatTextView = itemView.txtPeerCount
         val txtFileSize: AppCompatTextView = itemView.txtFileSize
         val txtTorrentSource: AppCompatTextView = itemView.txtTorrentSource
+        val txtAdToDownload: AppCompatTextView = itemView.txtHasAd
     }
 }
